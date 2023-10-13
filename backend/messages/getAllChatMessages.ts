ff8a5afbfd781756d46557246796690d768a1e56
jchack1 <55161597+jchack1@users.noreply.github.com>
2023-10-12 23:12:56 -0600

    initial setup of appsync and serverless

---
+++ backend/messages/getAllChatMessages.ts
@@ -0,0 +1,9 @@
+export function request() {
+  // return {};
+  return "hello world - request";
+}
+
+export function response(ctx) {
+  // return ctx.prev.result;
+  return "hello world - response";
+}
